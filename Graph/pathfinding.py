46ce828d8279aa25ca1b29811ab0c1accc6d6349
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2025-01-08 07:22:33 +0100

    added exploring function

--- Graph/pathfinding.py
+++ Graph/pathfinding.py
@@ -5,7 +5,53 @@ def astar(Graph):
     endNode = Graph.nodedict[Graph.endNode]
 
     # Initialize the open and closed lists
-    openSet = [startNode]
-    closedSet = []
+    Graph.openSet = [startNode]
+    Graph.closedSet = []
+
+    startNode.costFromStart = 0
+    startNode.totalCost = startNode.costFromStart + startNode.costToEnd
+    Graph.currentNode = startNode
+
+    explore_connected_nodes(Graph)
+
+
+
+
+
+def explore_connected_nodes(Graph):
+    #Get the current node
+    currentNode = Graph.currentNode
+
+    #remove current node from opens set
+    Graph.openSet.remove(currentNode)
+
+
+    Graph.closedSet.append(currentNode)
+
+    # Get all the neighboring nodes
+    for connection in currentNode.connections:
+        neighbor = connection.node1 if connection.node1 != currentNode else connection.node2
+        # If the neighbor is already in the closed set, skip it
+
+        if neighbor in Graph.closedSet:
+            continue
+
+        #add neighbor to open set
+        Graph.openSet.append(neighbor)
+
+        neighbor.costFromStart = currentNode.costFromStart + connection.weight
+        neighbor.totalCost = neighbor.costFromStart + neighbor.costToEnd
+
+        #print results
+        print(f"node {neighbor.id} has a cost of {neighbor.totalCost}")
+
+    #find the node with the lowest cost
+    lowestCostNode = min(Graph.openSet, key=lambda node: node.totalCost)
+
+    print(f"node {lowestCostNode.id} has the lowest cost of {lowestCostNode.totalCost}")
+
+
+
+
 
   
