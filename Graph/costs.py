910adda554b733e19158a45cdc27aed2cb09f477
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2025-01-12 08:42:20 +0100

    fixed mistake in cost calculation

--- Graph/costs.py
+++ Graph/costs.py
@@ -1,14 +1,14 @@
 
 def calculateCostsToEnd(Graph):
     endNode = Graph.nodedict[Graph.endNode]  # get the end node from the graph
-    EndPosition = (endNode.position[0] / 20, endNode.position[1] /20)
+    EndPosition = (endNode.position[0] / 10, endNode.position[1] /10)
     for node in Graph.nodedict.values():#
-        nodePosition = (node.position[0] / 20, node.position[1] / 20)
+        nodePosition = (node.position[0] / 10, node.position[1] / 10)
         node.costToEnd = round(abs(EndPosition[0] - nodePosition[0]) + abs(EndPosition[1] - nodePosition[1]))
 
 def calculateCostToEnd(Node, endNode):
-    EndPosition = (endNode.position[0] / 20, endNode.position[1] / 20)
-    Node.costToEnd = round(abs(EndPosition[0] - Node.position[0] / 20) + abs(EndPosition[1] - Node.position[1] / 20))
+    EndPosition = (endNode.position[0] / 10, endNode.position[1] / 10)
+    Node.costToEnd = round(abs(EndPosition[0] - Node.position[0] / 10) + abs(EndPosition[1] - Node.position[1] / 10))
 
 def resetAllCosts(Graph):
     for node in Graph.nodedict.values():
